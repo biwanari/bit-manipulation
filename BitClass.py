ba170aac052347092cedf9076912bf579ce3b87c
biwanari <andrewraieta@gmail.com>
2021-02-04 23:48:36 +0100

    ''

--- BitClass.py
+++ BitClass.py
@@ -3,13 +3,31 @@ import math
 class BitClass:
 
     #costruttore
-    def __init__(self,segno,esponente,mantissa):
-        self._segno = segno
-        self._esponente = esponente
-        self._mantissa = mantissa
+    def __init__(self, segno, esponente, mantissa):
+        #extrabit per il segno
+        self.segno     =  ""
+        self.esponente =  ""
+        self.mantissa  =  ""
+        if segno == "-":
+            self.segno = 1
+        elif segno == "+":
+            self.segno = 0
+         
+        self.segno = "?"
+
+        if all(c in '01' for c in esponente):
+            self.esponente = int(esponente)
+        
+        
+        self.esponente = "?"
+        if all(c in '01' for c in mantissa):    
+            self.mantissa  = int(mantissa)
+        self.mantissa = "?"
+        
+
     
     def __str__(self):
-        return f"{self.segno},{self.esponente},{self.mantissa}"
+        return f"   S    E   M \n < {self.segno} , {self.esponente} , {self.mantissa} > "
     def getSegno(self):
         return self.segno
     def getEsponente(self):
@@ -18,14 +36,9 @@ class BitClass:
         return self.mantissa
     def setSegno(self,value):
         self.segno = value
-    
-        
-        
-        
-    
 
+    def setEsponente(self,nuovo_esponente):
+        self.esponente = nuovo_esponente
     
-    def fromFloatToBin(self, floating_data):
-        pass
-
-
+    def setMantissa(self,nuova_mantissa):
+        self.mantissa = nuova_mantissa
